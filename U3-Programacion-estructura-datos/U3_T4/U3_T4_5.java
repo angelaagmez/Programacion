afc3b9cd6ebb25e602bd80ecda98fd0d5ac830b4
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2020-12-29 20:24:05 +0100

    Update U3_T4_5.java

--- U3-Programacion-estructura-datos/U3_T4/U3_T4_5.java
+++ U3-Programacion-estructura-datos/U3_T4/U3_T4_5.java
@@ -7,6 +7,43 @@ package com.abg;
 
 public class U3_T4_5 {
     public static void main(String[] args) {
+        int[][] num = new int[10][10];
+        long suma = 0;
+        int cont = 0, max = 0, min = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                num[i][j] = (int) (Math.random()*100)+200;
+
+                if (i==j) {
+                    suma += num[i][j];
+                    cont++;
+
+                    if (i == 0 && j == 0) {
+                        max = num[i][j];
+                        min = num[i][j];
+                    } else {
+                        if (max < num[i][j]) {
+                            max = num[i][j];
+                        }
+                        if (min > num[i][j]) {
+                            min = num[i][j];
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(Arrays.toString(num[i]));
+        }
+
+        System.out.println();
+
+        System.out.println("La suma es "+suma);
+        System.out.println("La media es "+(double)suma/cont);
+        System.out.println("El maximo es "+max);
+        System.out.println("El minimo es "+min);
 
     }
 }
